ea790d4bf220409339b1196ac8140e9459635172
Chino Franco <chino.franco@gmail.com>
2023-01-01 02:06:16 +0900

    Moved main script to outer folder

---
+++ main.py
@@ -0,0 +1,21 @@
+# Base libraries
+import os
+import time
+from argparse import ArgumentParser
+
+# NeRF library
+import nerf
+
+
+def config() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--source", "-s",
+        type=str, required=True,
+        help="The source directory to get array data from"
+    )
+    
+
+if __name__=="__main__": 
+    parser = config()
+    args = parser.parse_args()
